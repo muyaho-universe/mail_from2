6c552f71f341debab2898d2f6fcdb25c47db9091
muyaho-universe <kimdaesk@gmail.com>
2022-07-17 16:31:13 +0900

    Follow up step 2

---
+++ lib/login.dart
@@ -0,0 +1,105 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+import 'home.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+
+  CollectionReference database = FirebaseFirestore.instance.collection('user');
+  late QuerySnapshot querySnapshot;
+
+  Future<UserCredential> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.fromLTRB(20,0,20,0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/icon/icon.png', width: 220,),
+            SizedBox(height: 50,),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 70),
+              child: OutlinedButton(
+                onPressed: () async {
+                  final UserCredential userCredential = await signInWithGoogle();
+
+                  User? user = userCredential.user;
+
+                  if (user != null) {
+                    int i;
+                    querySnapshot = await database.get();
+
+                    for (i = 0; i < querySnapshot.docs.length; i++) {
+                      var a = querySnapshot.docs[i];
+
+                      if (a.get('uid') == user.uid) {
+                        break;
+                      }
+                    }
+
+                    if (i == (querySnapshot.docs.length)) {
+                      database.doc(user.uid).set({
+                        'email': user.email.toString(),
+                        'name': user.displayName.toString(),
+                        'uid': user.uid,
+                      });
+                    }
+                    if (user != null){
+                      Get.to(Login());
+                    }
+                    Get.to(HomePage());
+                  }
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset('assets/icon/google_logo_icon.png', width: 20,),
+                    Text("  Sign in with Google",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),),
+
+                  ],
+                ),
+                style: OutlinedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(vertical: 20),
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                ),),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
