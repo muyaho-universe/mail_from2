9b92f257f044b6446cc2e47fda83cf0120aa421a
muyaho-universe <kimdaesk@gmail.com>
2022-07-18 19:59:57 +0900

    Complete step 4

--- lib/mail/mail_list/mail_list.dart
+++ lib/mail/mail_list/mail_list.dart
@@ -3,9 +3,11 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:mail_from2/util/size.dart';
 
 import '../mail.dart';
 import '../mail_content/show_mail.dart';
+import '../mail_content/temporaryStorage_modal.dart';
 import 'mail_card.dart';
 
 class MailList extends StatefulWidget {
@@ -35,58 +37,84 @@ class _MailListState extends State<MailList> {
           }
           List<Mail> mailDocs = [];
 
-          if(snapshot.hasData){
-            for(int i = 0; i < snapshot.data!.docs.length; i++){
+          if (snapshot.hasData) {
+            for (int i = 0; i < snapshot.data!.docs.length; i++) {
               var one = snapshot.data!.docs[i];
-              if(one.get('writer') == user!.email ||
-                  one.get('recipient') == user!.email){
+              if (one.get('writer') == user!.email ||
+                  one.get('recipient') == user!.email) {
                 Timestamp t = one.get('time');
                 print(one.get('time'));
 
-                String time = DateTime.fromMicrosecondsSinceEpoch(t.microsecondsSinceEpoch).toString().split(" ")[0];
+                String time = DateTime.fromMicrosecondsSinceEpoch(
+                        t.microsecondsSinceEpoch)
+                    .toString()
+                    .split(" ")[0];
                 print(time);
                 time = time.replaceAll("-", ".");
 
-                Mail mail = Mail(one.id, one.get('title'), one.get('content'), one.get('writer'), one.get('recipient'), time, one.get('read'), one.get('sent'));
+                Mail mail = Mail(
+                    one.id,
+                    one.get('title'),
+                    one.get('content'),
+                    one.get('writer'),
+                    one.get('recipient'),
+                    time,
+                    one.get('read'),
+                    one.get('sent'));
 
-                if(widget.title == '보낸 편지함'){
-                  if(one.get('writer') == user!.email){
-                    Mail myMail = Mail(one.id, one.get('title'), one.get('content'), one.get('writer'), one.get('recipient'), time, one.get('read'), one.get('sent'));
+                if (widget.title == '보낸 편지함') {
+                  if (one.get('writer') == user!.email &&
+                      one.get('sent') == true) {
+                    Mail myMail = Mail(
+                        one.id,
+                        one.get('title'),
+                        one.get('content'),
+                        one.get('writer'),
+                        one.get('recipient'),
+                        time,
+                        one.get('read'),
+                        one.get('sent'));
                     mailDocs.add(myMail);
                   }
-                }
-                else if(widget.title == '받은 편지함'){
-                  if(one.get('recipient') == user!.email){
+                } else if (widget.title == '받은 편지함' &&
+                    one.get('sent') == true) {
+                  if (one.get('recipient') == user!.email) {
                     mailDocs.add(mail);
                   }
-                }
-                else if(widget.title == '임시 저장'){
-                  if(one.get('writer') == user!.email &&
-                      one.get('sent') == false){
+                } else if (widget.title == '임시 저장') {
+                  if (one.get('writer') == user!.email &&
+                      one.get('sent') == false) {
                     mailDocs.add(mail);
                   }
-                }
-                else if(widget.title == '모든 메일'){
+                } else if (widget.title == '모든 메일') {
                   mailDocs.add(mail);
-                }
-                else{
-
-                }
+                } else {}
               }
             }
           }
           return ListView.builder(
               itemCount: mailDocs.length,
-              itemBuilder: (context, index){
+              itemBuilder: (context, index) {
                 return GestureDetector(
-                  onTap: (){
-                    Get.to(ShowMail(mailDocs[index]));
+                  onTap: () {
+                    widget.title == "임시 저장"
+                        ? showModalBottomSheet(
+                            isScrollControlled: true,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.vertical(
+                              top: Radius.circular(10),
+                            )),
+                            context: context,
+                            builder: (context) => Container(
+                              height: getScreenHeight(context) * 0.9,
+                              child: ShowTemporaryStorage(mailDocs[index]),
+                            ),
+                          )
+                        : Get.to(ShowMail(mailDocs[index]));
                   },
                   child: MailCard(mailDocs[index]),
                 );
-              }
-
-          );
+              });
         });
   }
 }
--- lib/mail/mail_list/mail_screen.dart
+++ lib/mail/mail_list/mail_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../../util/size.dart';
+import '../sendMail_modal.dart';
 import 'mail_list.dart';
 
 class MailScreen extends StatefulWidget {
@@ -16,35 +18,61 @@ class _MailScreenState extends State<MailScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title.toString(),
-          style: TextStyle(color: Colors.black, fontSize: 15, fontWeight: FontWeight.w600),),
+        title: Text(
+          widget.title.toString(),
+          style: TextStyle(
+              color: Colors.black, fontSize: 15, fontWeight: FontWeight.w600),
+        ),
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
         iconTheme: IconThemeData(
-          color: Colors.black,//색변경
+          color: Colors.black, //색변경
           size: 15,
         ),
-
       ),
       body: Container(
-          child: Column(
-            children: [
-              Expanded(child: MailList(widget.title),),
-            ],
-          )
-      ),
-      bottomNavigationBar:BottomAppBar(
-        child: Row(
+        child: Column(
           children: [
-            IconButton(onPressed: (){
-
-            }, icon: Icon(CupertinoIcons.equal_circle, color: Colors.blueAccent,)),
-            Spacer(),
-            IconButton(onPressed: (){
-
-            }, icon: Icon(CupertinoIcons.pencil_outline, color: Colors.blueAccent,)),
+            Expanded(
+              child: MailList(widget.title),
+            ),
           ],
         ),
-      ) ,
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                CupertinoIcons.equal_circle,
+                color: Colors.blueAccent,
+              ),
+            ),
+            Spacer(),
+            IconButton(
+              onPressed: () {
+                showModalBottomSheet(
+                  isScrollControlled: true,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(10)
+                      ),
+                    ),
+                    context: context,
+                    builder: (context) => Container(
+                      height: getScreenHeight(context) * 0.9,
+                      child: MailModal(),
+                    ),
+                );
+              },
+              icon: Icon(
+                CupertinoIcons.pencil_outline,
+                color: Colors.blueAccent,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
